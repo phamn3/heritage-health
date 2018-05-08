a9f94688fa8ad00fd932741b16bb7fcfaf88d1ca
fatmat <31808205+fatmat@users.noreply.github.com>
2018-05-08 10:07:38 -0400

    final code

--- Agg_Y1.R
+++ Agg_Y1.R
@@ -10,7 +10,10 @@ library(readr)
 if(!(require(dplyr))) install.packages("dplyr")
 if(!(require(ggplot2))) install.packages("ggplot2")
 if(!require(ggcorrplot)) install.packages("ggcorrplot")
-
+library(purrr)
+library(tidyr)
+library(Metrics)
+library(glmnet)
 
 ############### YEAR 1 ################################################################
 #######################################################################################
@@ -28,6 +31,17 @@ sapply(df1, function(x) sum(is.na(x))) # of missing variables
 View(df1)
 
 length(unique(MemberID)) #76038 unique patients
+
+## plot of missing values
+df1 %>% 
+  summarise_all(funs(sum(is.na(.)))) %>% 
+  gather %>% 
+  ggplot(aes(x = reorder(key, value), y = value)) + geom_bar(stat = "identity") +
+  coord_flip() +
+  xlab("variable") +
+  ylab("Absolute number of missings")
+
+
 
 ################# Data Manipulation #####################
 # fixing DSFS - days since first claim
@@ -282,14 +296,14 @@ hist((df1_Agg$LogDaysInHospital), col="darkgreen", main="Agg_DIH", xlab="Log_DIH
 
 colnames(df1_Agg)
 
-#### testing a base model1 with DIH = 0
+#### testing a base model1 with DIH = 0 , results in rmse = 1.3745
 rmse(log(0 + 1), df2_Agg$LogDaysInHospital)
 
-# Testing a base model2 with DIH_Y2 = mean(DIH_Y1)
+# Testing a base model2 with DIH_Y2 = mean(DIH_Y1), results in rmse = 1.2825
 rmse(mean(df1_Agg$LogDaysInHospital, na.rm = TRUE), df2_Agg$LogDaysInHospital)
 
 
-####  testing linear model with few features, not improving the rmse 
+####  testing linear model with few features, not improving the rmse , can be ignored
 n_data<- df1_Agg[c(1,21:66,108)]
 m_data<- df2_Agg[c(1, 21:66,108)]
 linear.model <- lm(LogDaysInHospital ~. , data=n_data)
@@ -306,21 +320,55 @@ rmse(o,m)
 
 
 
-#### linear model using LogDaysInHospital rmse = 1.1872
+#### linear model using LogDaysInHospital rmse = 1.1872, R squared very low = 0.1381
 
 linear.model1 <- lm(LogDaysInHospital ~. , data=df1_Agg[-7])
 summary(linear.model1)
 
-pred1 <- predict(linear.model1, newdata=df2_Agg[,-c(7,108)])
-pred1
+pred_lm <- predict(linear.model1, newdata=df2_Agg[,-c(7,108)])
+pred_lm
 
 
-m<-pred1
+m<-pred_lm
 o<-df2_Agg[108]
-
 rm<- (sqrt(mean((m - o)^2)))
-
 rmse(o,m)
+
+# tranforming back to normal number by taking "exp" of the predictors.
+dihY2_lm<- floor(exp(pred_lm) - 1)
+summary(dihY2_lm)
+###################### end linear regression ######################
+
+############### lasso regression ############## rmse = 1.2598
+library(glmnet)
+x<- as.matrix(df1_Agg[,-c(7,108)])
+y<- df1_Agg$LogDaysInHospital
+x1<- as.matrix(df2_Agg[,-c(7,108)])
+y1<-df2_Agg$LogDaysInHospital
+
+lambdas <- 10^seq(3, -2, by = -.1)
+
+lasso.mod <- glmnet(x, y, alpha = 1)
+lasso.pred <- predict(lasso.mod, s = lambdas, newx = x1)
+rm<- (sqrt(mean((lasso.pred-y1)^2)))
+
+
+# tranforming back to normal number by taking "exp" of the predictors.
+dihY2_lasso<-as.vector(floor(exp(lasso.pred) - 1))
+summary(dihY2_lasso)
+
+#################### end lasso #############################
+
+#################### glm #################### rmse = 1.3209
+# GBM
+mod_glm <- glm(LogDaysInHospital ~ ., family = "poisson", data = df1_Agg[, -c(1, 7)])
+pred_glm_orig <- predict(mod_glm, df2_Agg[,-c(1, 7,108)])
+pred_glm <- log(ifelse(pred_glm_orig < 0, 0, pred_glm_orig) + 1)
+rmse(pred_glm, df2_Agg$LogDaysInHospital)
+
+# tranforming back to normal number by taking "exp" of the predictors.
+dihY2_glm<- floor(exp(pred_glm) - 1)
+summary(dihY2_glm)
 
 
 
@@ -363,30 +411,29 @@ sse <- sum((y_predicted - y1)^2)
 rsq <- 1 - sse / sst
 rsq
 
+# tranforming back to normal number by taking "exp" of the predictors.
+dihY2_glm<- floor(exp(y_predicted) - 1)
+summary(dihY2_glm)
+
+
+################ end ridge regression ################
 
 
 
-############### lasso regression ############## rmse = 1.2598
-library(glmnet)
-x<- as.matrix(df1_Agg[,-c(7,108)])
-y<- df1_Agg$LogDaysInHospital
-x1<- as.matrix(df2_Agg[,-c(7,108)])
-y1<-df2_Agg$LogDaysInHospital
-
-lambdas <- 10^seq(3, -2, by = -.1)
-
-lasso.mod <- glmnet(x, y, alpha = 1)
-lasso.pred <- predict(lasso.mod, s = lambdas, newx = x1)
-rm<- (sqrt(mean((lasso.pred-y1)^2)))
 
 
-############ glm #################### rmse = 1.3209
-# GBM
-mod_glm <- glm(LogDaysInHospital ~ ., family = "poisson", data = df1_Agg[, -c(1, 7)])
-pred_glm_orig <- predict(mod_glm, df2_Agg[,-c(1, 7,108)])
-pred_glm <- log(ifelse(pred_glm_orig < 0, 0, pred_glm_orig) + 1)
-rmse(pred_glm, df2_Agg$LogDaysInHospital)
 
+
+
+### ensemble model with weighted average of Lasso, glm and Linear model improves rmse slighlty from 
+## lowest rmse we get from linear model of  1.1872  to 1.186385
+pred_ensemble<- 0.1*lasso.pred  + 0.04*pred_glm + 0.86*pred_lm
+pred_ensemble
+rmse(pred_ensemble, df2_Agg$LogDaysInHospital)
+
+normal_pred<- as.vector(floor(exp(pred_ensemble) - 1))
+normal_pred
+summary(normal_pred)
 
 
 
